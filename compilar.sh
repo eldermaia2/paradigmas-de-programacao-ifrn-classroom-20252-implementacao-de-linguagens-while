a21cee280affcb8a7836b330ed32179e832d9501
eldermaia2 <165786848+eldermaia2@users.noreply.github.com>
2026-04-29 16:37:07 +0000

    build: adiciona verificação de código deprecated com javac

--- compilar.sh
+++ compilar.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-java -jar ./lib/antlr-4.13.1-complete.jar -package plp.enquanto.parser  ./src/plp/enquanto/parser/Enquanto.g4
-javac -cp ./lib/antlr-runtime-4.13.1.jar -d bin ./src/plp/enquanto/parser/*.java ./src/plp/enquanto/*.java
+# Gera o parser com ANTLR
+java -jar ./lib/antlr-4.13.1-complete.jar -package plp.enquanto.parser ./src/plp/enquanto/parser/Enquanto.g4
+
+# Cria pasta bin (evita erro se não existir)
+mkdir -p bin
+
+# Compila com detecção de código deprecated (ESSENCIAL)
+javac -Xlint:deprecation -Werror \
+-cp ./lib/antlr-runtime-4.13.1.jar \
+-d bin \
+$(find ./src -name "*.java")
+
+# Copia dependência para o jar final
 cp ./lib/antlr-runtime-4.13.1.jar while.jar
+
+# Empacota aplicação
 jar --update --file ./while.jar --main-class plp.enquanto.Principal -C bin plp
